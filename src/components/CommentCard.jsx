107004393b7c204625fe6fae45a85fd8fac5dae9
Valentina <smeu.valentina33@gmail.com>
2023-01-10 12:02:54 +0000

    add list of comments and commentCard component

---
+++ src/components/CommentCard.jsx
@@ -0,0 +1,13 @@
+export const CommentCard = ({ author, body, votes, created_at }) => {
+  return (
+    <div className="Comment_card">
+      <p className="Comment_date">{created_at}</p>
+      <article>{body}</article>
+      <div className="Comment_author_votes"> 
+      <p><b>{author}</b></p>
+      <p>Votes: <b>{votes}</b> </p>
+      </div>
+     
+    </div>
+  );
+};
